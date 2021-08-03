44d920d8b52b54959725555f295caa760dd9f8f5
Florian Contival <11033778+KindVador@users.noreply.github.com>
2021-08-03 21:48:42 +0200

    add new plugin structure

---
+++ plotjuggler_plugins/DataLoadCustomCsv/dataload_custom_csv.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <QObject>
+#include <QtPlugin>
+#include "PlotJuggler/dataloader_base.h"
+
+using namespace PJ;
+
+class DataLoadCustomCsv : public DataLoader
+{
+  Q_OBJECT
+  Q_PLUGIN_METADATA(IID "facontidavide.PlotJuggler3.DataLoader")
+  Q_INTERFACES(PJ::DataLoader)
+
+public:
+  DataLoadCustomCsv();
+  virtual const std::vector<const char*>& compatibleFileExtensions() const override;
+
+  virtual bool readDataFromFile(PJ::FileLoadInfo* fileload_info, PlotDataMapRef& destination) override;
+
+  virtual ~DataLoadCustomCsv();
+
+  virtual const char* name() const override
+  {
+    return "DataLoad Custom CSV";
+  }
+
+  virtual bool xmlSaveState(QDomDocument& doc, QDomElement& parent_element) const override;
+
+  virtual bool xmlLoadState(const QDomElement& parent_element) override;
+
+protected:
+  QSize parseHeader(QFile* file, std::vector<std::string>& ordered_names);
+
+private:
+  std::vector<const char*> _extensions;
+
+  std::string _default_time_axis;
+};
+
+
